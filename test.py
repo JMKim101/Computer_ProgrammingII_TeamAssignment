f9ba1ab844a400522e2658b6552485200043c1d0
JaeMin Kim <jeffthehero038@gmail.com>
2024-11-30 17:00:57 +0900

    Details 수정 완료.

--- test.py
+++ test.py
@@ -2,71 +2,99 @@ import matplotlib.pyplot as plt
 import csv
 import matplotlib
 
-
 matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 
-data = r"C:\Users\uniba\Downloads\과제 6. 팀프로젝트\20231231.csv"
+student_dist_by_subject = "20231231.csv"
 
-available_subjects = []
+# 우선 큰 과목 영역(국수탐) 정하기
+subject_areas = []
 
-with open(data, mode='r', encoding='cp949') as file:
+with open(student_dist_by_subject, mode='r', encoding='cp949') as file:
     reader = csv.reader(file)
     next(reader)  
 
     for row in reader:
-        if row[0] not in available_subjects:
-            available_subjects.append(row[0])  
+        if row[0] not in subject_areas:
+            subject_areas.append(row[0])  
 
 
-print("과목:")
-for idx, subject in enumerate(available_subjects, start=1):
+print("과목 영역:")
+for idx, subject in enumerate(subject_areas, start=1):
     print(f"{idx}: {subject}")
 
 while True:
     try:
-        subject_choice = int(input("과목 번호를 선택하세요: "))
-        if 1 <= subject_choice <= len(available_subjects):
-            selected_subject = available_subjects[subject_choice - 1]
+        subject_area_choice = int(input("과목 영역에 해당하는 번호를 선택하세요: "))
+        if 1 <= subject_area_choice <= len(subject_areas):
+            selected_subject_area = subject_areas[subject_area_choice - 1]
             break
         else:
-            print(f"1부터 {len(available_subjects)} 사이의 번호를 입력해주세요.")
+            print(f"1부터 {len(subject_areas)} 사이의 번호를 입력해주세요.")
     except ValueError:
         print("올바른 숫자를 입력해주세요.")
+        
+# 그 후, 과목 영역 중 자세한 과목 유형 선택하기
+        
+subject_types = []
+        
+with open(student_dist_by_subject, mode='r', encoding='cp949') as file:
+    reader = csv.reader(file)
+    next(reader)
+    
+    for row in reader:
+        if row[0] == selected_subject_area:
+            if row[1] not in subject_types:
+                subject_types.append(row[1])
+                
+print("과목 유형:")
+for idx, subject in enumerate(subject_types, start=1):
+    print(f"{idx}: {subject}")
+
+while True:
+    try:
+        subject_type_choice = int(input("과목 유형에 해당하는 번호를 선택하세요: "))
+        if 1 <= subject_type_choice <= len(subject_types):
+            selected_subject_type = subject_types[subject_type_choice - 1]
+            break
+        else:
+            print(f"1부터 {len(subject_areas)} 사이의 번호를 입력해주세요.")
+    except ValueError:
+        print("올바른 숫자를 입력해주세요.")
+                
+                
+# 그래프 출력하기
 
 standard_scores = []
-male_num = []
-female_num = []
+male_student_counts = []
+female_student_counts = []
 
-with open(data, mode='r', encoding='cp949') as file:
+with open(student_dist_by_subject, mode='r', encoding='cp949') as file:
     reader = csv.reader(file)
     next(reader)  
 
     for row in reader:
-        if row[0] == selected_subject:
+        if row[1] == selected_subject_type:
             standard_scores.append(int(row[2]))  
-            male_num.append(int(row[3]))        
-            female_num.append(int(row[4]))      
+            male_student_counts.append(int(row[3]))        
+            female_student_counts.append(int(row[4]))      
 
 
-sorted_data = sorted(zip(standard_scores, male_num, female_num))
-sorted_standard_scores, sorted_male_scores, sorted_female_scores = zip(*sorted_data)
+sorted_data = sorted(zip(standard_scores, male_student_counts, female_student_counts))
+sorted_standard_scores, sorted_male_student_counts, sorted_female_student_counts = zip(*sorted_data)
 
 
 plt.figure(figsize=(10, 6))
 
-plt.plot(sorted_standard_scores, sorted_male_scores, label='남자', color='blue', linestyle='-', linewidth=2)
-plt.plot(sorted_standard_scores, sorted_female_scores, label='여자', color='pink', linestyle='-', linewidth=2)
+x_values = sorted_standard_scores
+y_male = sorted_male_student_counts
+y_female = sorted_female_student_counts
 
-plt.title(f'2024학년도 수능 {selected_subject} 성적 분포')
+plt.plot(x_values, y_male, label='남자', color='royalblue', linestyle='-', linewidth=2)
+plt.plot(x_values, y_female, label='여자', color='lightpink', linestyle='-', linewidth=2)
+
+plt.title(f'2024학년도 수능 {selected_subject_type} 성적 분포')
 plt.xlabel('표준점수')
 plt.ylabel('학생 수')
 
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
